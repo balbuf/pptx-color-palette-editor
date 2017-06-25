7182d80067729769b0cbb0f3d78c399fc4121b47
Stephen Beemsterboer <balbuf@gmail.com>
2017-06-25 00:58:17 -0400

    Save bundle to docs folder instead

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const config = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     filename: 'pptx-color-palette-editor.bundle.js'
   },
   module: {
